9a65520c70f313bfa384655e81d344e67f8972cc
yulinZ <1973329248@qq.com>
2023-07-03 15:02:57 +0800

    1. add:目录拖拽功能

--- src/routes/article/[id]/index.tsx
+++ src/routes/article/[id]/index.tsx
@@ -1,23 +1,12 @@
-import {
-  component$,
-  useSignal,
-  useStyles$,
-  $,
-  useOnDocument,
-  useOnWindow,
-} from "@builder.io/qwik";
-import {
-  type DocumentHead,
-  useLocation,
-  routeLoader$,
-} from "@builder.io/qwik-city";
-import { getArticleDetils } from "~/api";
-import styles from "./index.css?inline";
-import markdownIt from "markdown-it";
-import hljs from "highlight.js";
-import toc from "markdown-it-toc-done-right";
-import anchor from "markdown-it-anchor";
-import {on} from "../../../utils/dom"
+import { component$, useSignal, useStyles$, $, useOnDocument, useOnWindow } from '@builder.io/qwik';
+import { type DocumentHead, useLocation, routeLoader$ } from '@builder.io/qwik-city';
+import { getArticleDetils } from '~/api';
+import styles from './index.css?inline';
+import markdownIt from 'markdown-it';
+import hljs from 'highlight.js';
+import toc from 'markdown-it-toc-done-right';
+import anchor from 'markdown-it-anchor';
+import { on, off } from '../../../utils/dom';
 
 const useData = routeLoader$(async (requestEvent) => {
   let response: any = null;
@@ -37,88 +26,149 @@ export default component$(() => {
   const dataList: any = useData();
   const article = dataList.value.data[0];
   const markdownRef = useSignal<Element>();
+  const isDrag = useSignal(false);
 
+  // 窗口缩放
   useOnWindow(
-    "resize",
+    'resize',
     $(() => {
       if (window.innerWidth < 1200) {
-        const tocListDom = document.querySelector(".table-of-contents");
+        const tocListDom = document.querySelector('.table-of-contents');
         const disStyle = (tocListDom as HTMLElement).style;
-        disStyle.display = "none";
+        disStyle.display = 'none';
       }
     })
   );
 
+  // 窗口加载
+  useOnWindow(
+    'load',
+    $(() => {
+      const divs = document.querySelectorAll('.table-of-contents');
 
-  useOnDocument('mousedown',$((event:Event)=>{
- 
-    const target = event.target as HTMLElement;
+      const elem = document.createElement('button');
 
-    const e = event as Event
-    function move(e){            
-      e = e || window.event            
-      var left = e.clientX - BoxX            
-      var top = e.clientY - BoxY            
-      Box.style.left = left + 'px'            
-      Box.style.top = top + 'px'            
-      //限制左上角边缘距离            
-      if(left<=0){                
-          left=0;            
-      }            
-      Box.style.left = left + "px"            
-      if(top<=0){                
-          top=0            
-      }           
-      Box.style.top = top + "px"            
-      //限制右下角边缘距离            
-      var maxL = window.innerWidth - Box.offsetWidth;            
-      var maxT = window.innerHeight - Box.offsetHeight;            
-      if(left>=maxL){                
-          left = maxL;            
-      }            
-      Box.style.left = left + "px"            
-      if(top>=maxT){                
-          top = maxT;            
-      }            
-      Box.style.top = top + "px"       
-  }  
-      // 点击的是目录按钮
-      if (target.className === "toc-btn") {
-        // e = e || window.event// 获取事件对象                
-        const BoxX = e.offsetX// 获取鼠标点击时到div的顶部和左边的距离         
-        const BoxY = e.offsetY       
-        console.log(BoxX,BoxY)
-        // on(document,'mousemove',move) 
-      }
-  }))
+      console.log('load');
+      elem.className = 'toc-btn';
+      elem.innerText = '目录';
+      const pDiv = document.createElement('div');
+      pDiv.className = 'toc-btn-p';
 
+      divs.forEach(function (div: any, index) {
+        div.parentNode.replaceChild(pDiv, div);
+
+        pDiv.appendChild(div);
+      });
+
+      elem.append(pDiv);
+      document.body.append(elem);
+    })
+  );
+
+  // 鼠标按下
   useOnDocument(
-    "click",
+    'mousedown',
+    $((event: Event) => {
+      const target = event.target as HTMLElement;
+      const e = (event || window.event) as MouseEvent;
+      isDrag.value = true;
+      // 点击的是目录按钮
+      if (target.className === 'toc-btn') {
+        // dom 边缘到顶部 和左侧的距离
+        const x = e.offsetX;
+        const y = e.offsetY; //得到小段的偏移
+
+        document.onmousemove = (event: MouseEvent) => {
+          let xx: any = event.clientX - x;
+          let yy: any = event.clientY - y;
+          // 获取可视区域的width height
+          const bodyW: number = document.documentElement.clientWidth;
+          const bodyH: number = document.documentElement.clientHeight;
+          const tocDom = document.querySelector('.table-of-contents') as HTMLElement;
+
+          if (xx <= 0) {
+            xx = 0;
+          }
+
+          if (xx >= bodyW - target.offsetWidth) {
+            xx = bodyW - target.offsetWidth;
+          }
+
+          if (yy <= 0) {
+            yy = 0;
+          }
+          console.log(xx, bodyH, target.offsetHeight);
+          if (yy >= bodyH - target.offsetHeight) {
+            yy = bodyH - target.offsetHeight;
+          }
+
+          target.style.left = xx + 'px';
+          // tocDom.style.left = xx + 'px';
+          target.style.top = yy + 'px';
+          // tocDom.style.top = yy + 'px';
+        };
+        document.onmouseup = (event: Event) => {
+          let xx: any = event.clientX - x;
+          let yy: any = event.clientY - y;
+          // 获取可视区域的width height
+          const bodyW: number = document.documentElement.clientWidth;
+          const bodyH: number = document.documentElement.clientHeight;
+          const tocDom = document.querySelector('.table-of-contents') as HTMLElement;
+
+          console.log(xx, bodyW / 2);
+          if (xx <= bodyW / 2) {
+            target.style.left = '0px';
+            tocDom.style.right = 'auto';
+          } else {
+            target.style.right = '0px';
+            target.style.left = 'auto';
+            tocDom.style.right = '0px';
+            tocDom.style.left = 'auto';
+          }
+
+          document.onmousemove = null;
+          document.onmouseup = null;
+          isDrag.value = false;
+        };
+
+        // 避免全选文字造成的bug，在这里阻止默认行为
+        if (e.preventDefault) {
+          e.preventDefault();
+        } else {
+          return false;
+        }
+      }
+    })
+  );
+
+  // 点击
+  useOnDocument(
+    'click',
     $((event: Event) => {
       const target = event.target as HTMLElement;
       // 点击的是目录按钮
-      if (target.className === "toc-btn") {
-        const tocListDom = document.querySelector(".table-of-contents");
+      if (target.className === 'toc-btn' && !isDrag.value) {
+        const tocListDom = document.querySelector('.table-of-contents');
         const disStyle = (tocListDom as HTMLElement).style;
-        if (!disStyle.display || disStyle.display === "block") {
-          disStyle.display = "none";
+        if (!disStyle.display || disStyle.display === 'block') {
+          disStyle.display = 'none';
         } else {
-          disStyle.display = "block";
+          disStyle.display = 'block';
         }
       }
 
-      const copyId: any = target.getAttribute("data-clipboard-target");
+      const copyId: any = target.getAttribute('data-clipboard-target');
       // code 复制功能
       if (copyId) {
         const element = document.querySelector(copyId);
         element.select();
         element.setSelectionRange(0, element.value.length);
-        document.execCommand("copy");
+        document.execCommand('copy');
 
         // 修改copy btn 的提示 2s 后还原
-        target.innerText = "copied";
+        target.innerText = 'copied';
         setTimeout(() => {
-          target.innerText = "copy";
+          target.innerText = 'copy';
         }, 2000);
       }
     })
@@ -130,8 +180,7 @@ export default component$(() => {
     typographer: true,
     highlight: function (str: string, lang: string) {
       //生成随机id 标识 用于复制功能
-      const codeIndex =
-        parseInt(Date.now() as any) + Math.floor(Math.random() * 10000000);
+      const codeIndex = parseInt(Date.now() as any) + Math.floor(Math.random() * 10000000);
 
       if (lang && hljs.getLanguage(lang)) {
         try {
@@ -145,32 +194,24 @@ export default component$(() => {
           // 添加自定义行号
           let html = lines
             .map((item, index) => {
-              return (
-                '<li><span class="line-num" data-line="' +
-                (index + 1) +
-                '"></span>' +
-                item +
-                "</li>"
-              );
+              return '<li><span class="line-num" data-line="' + (index + 1) + '"></span>' + item + '</li>';
             })
-            .join("");
-          html = '<ol style="overflow-x: auto;margin:0">' + html + "</ol>";
+            .join('');
+          html = '<ol style="overflow-x: auto;margin:0">' + html + '</ol>';
 
           // 添加代码语言
           if (lines.length) {
-            html =
-              '<b class="code-type"><span>Type:</span>' + lang + "</b>" + html;
+            html = '<b class="code-type"><span>Type:</span>' + lang + '</b>' + html;
           }
           // 添加复制标签
           html =
-            `<div class="code-copy"  data-clipboard-action="copy" data-clipboard-target="#copy${codeIndex}"> copy</div>` +
-            html;
+            `<div class="code-copy"  data-clipboard-action="copy" data-clipboard-target="#copy${codeIndex}"> copy</div>` + html;
           return (
             '<pre class="hljs article-pre" ><code style="padding:6px">' +
             html +
             `</code></pre><textarea style="position: absolute;top: -9999px;left: -9999px;z-index: -9999;" id="copy${codeIndex}">${str.replace(
               /<\/textarea>/g,
-              "&lt;/textarea>"
+              '&lt;/textarea>'
             )}</textarea>`
           );
         } catch (__) {
@@ -182,22 +223,16 @@ export default component$(() => {
       const lines = preCode.split(/\n/).slice(0, -1);
       let html = lines
         .map((item: any, index: number) => {
-          return (
-            '<li><span class="line-num" data-line="' +
-            (index + 1) +
-            '"></span>' +
-            item +
-            "</li>"
-          );
+          return '<li><span class="line-num" data-line="' + (index + 1) + '"></span>' + item + '</li>';
         })
-        .join("");
-      html = '<ol style="overflow-x: auto;margin:0">' + html + "</ol>";
+        .join('');
+      html = '<ol style="overflow-x: auto;margin:0">' + html + '</ol>';
       return (
         '<pre class="hljs"><code style="padding:6px">' +
         html +
         `</code></pre><textarea style="position: absolute;top: -9999px;left: -9999px;z-index: -9999;" id="copy${codeIndex}">${str.replace(
           /<\/textarea>/g,
-          "&lt;/textarea>"
+          '&lt;/textarea>'
         )}</textarea>`
       );
     },
@@ -207,27 +242,22 @@ export default component$(() => {
       // slugify: string => string,
       permalink: true,
       // renderPermalink: (slug, opts, state, permalink) => {},
-      permalinkClass: "header-anchor",
-      permalinkSymbol: "¶",
+      permalinkClass: 'header-anchor',
+      permalinkSymbol: '¶',
       permalinkBefore: true,
     })
-    .use(toc, { listType: "ul", itemClass: "toc-item" });
+    .use(toc, { listType: 'ul', itemClass: 'toc-item' });
 
   // const tocRender: any = md.render("\n\n${toc}\n" + article?.content);
-  const htmlStr: any = md.render("\n\n${toc}\n" + article?.content);
+  const htmlStr: any = md.render('\n\n${toc}\n' + article?.content);
 
   return (
     <div class="article-main">
-      {/* <div class="arc-toc"></div> */}
-      <button class="toc-btn">目录</button>
+      {/* <button class="toc-btn">目录</button> */}
       <article>
         <h1 class="arc-title">{article?.title}</h1>
         <div class="arc-bf">{article?.bf}</div>
-        <div
-          id="preview"
-          ref={markdownRef}
-          dangerouslySetInnerHTML={htmlStr}
-        ></div>
+        <div id="preview" ref={markdownRef} dangerouslySetInnerHTML={htmlStr}></div>
       </article>
     </div>
   );
@@ -241,30 +271,23 @@ export const head: DocumentHead = ({ resolveValue }) => {
     title: article?.title,
     meta: [
       {
-        name: "description",
+        name: 'description',
         content: article?.bf,
       },
       {
-        name: "keywords",
+        name: 'keywords',
         content:
-          article?.title +
-          " " +
-          "" +
-          article.group?.name +
-          " " +
-          "" +
-          article?.labels.map((item: any) => item?.name).join(" "),
+          article?.title + ' ' + '' + article.group?.name + ' ' + '' + article?.labels.map((item: any) => item?.name).join(' '),
       },
     ],
     links: [
       {
-        rel: "stylesheet",
-        name: "",
-        href: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/tokyo-night-dark.min.css",
-        integrity:
-          "sha512-dSQLLtgaq2iGigmy9xowRshaMzUHeiIUTvJW/SkUpb1J+ImXOPNGAI7ZC8V5/PiN/XN83B8uIk4qET7AMhdC5Q==",
-        referrerpolicy: "no-referrer",
-        crossorigin: "anonymous",
+        rel: 'stylesheet',
+        name: '',
+        href: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/tokyo-night-dark.min.css',
+        integrity: 'sha512-dSQLLtgaq2iGigmy9xowRshaMzUHeiIUTvJW/SkUpb1J+ImXOPNGAI7ZC8V5/PiN/XN83B8uIk4qET7AMhdC5Q==',
+        referrerpolicy: 'no-referrer',
+        crossorigin: 'anonymous',
         // <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/tokyo-night-dark.min.css" integrity="sha512-dSQLLtgaq2iGigmy9xowRshaMzUHeiIUTvJW/SkUpb1J+ImXOPNGAI7ZC8V5/PiN/XN83B8uIk4qET7AMhdC5Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
       },
     ],
